c5165fa05513209ed7bee10ae6b43e9bafb721e0
amh-137 <a.hergenhan@imperial.ac.uk>
2024-10-11 13:55:26 +0100

    Added basic file read

---
+++ README.md
@@ -0,0 +1,19 @@
+# STC-DRIFT-TIME
+
+Read STC.pdf for details.
+
+## Build C++
+```make```
+or
+```g++ main.cpp -o STC
+
+## Build LaTeX
+
+
+## Temp (Ideas)
+function to read in data
+vector class to handle the data? Or should i just use std::vector
+
+## To Do
+* Rewrite read_file to do chunk size reading of data
+* make sure using 4 spaces NOT tab (ctrl shift p convert indentation to spaces)
---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+// main.cpp
+// Alex Hergenhan 2024
+// 
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+int read_file(std::string fname, std::vector<double>& data); // returns 0 if success
+
+template <typename T>
+
+
+void vprint(std::vector<T> v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main(){
+
+    std::vector<double> data;
+
+    int stat {read_file("data/onetrack.raw", data)};
+    
+    if (stat != 0){
+        return 1;
+    }
+
+    vprint(data);
+
+    return 0;
+}
+
+
+int read_file(std::string fname, std::vector<double>& data) {
+    // check that file exists
+
+
+    // open file
+    std::ifstream inp(fname, std::ios::binary);
+
+    if (!inp) {
+        std::cerr << "Failed to open file: " << fname << std::endl;
+        return 1;
+    }
+
+
+    // Get the size of the file
+    inp.seekg(0, std::ios::end);
+    std::streamsize size = inp.tellg();
+    inp.seekg(0, std::ios::beg);
+
+    // Create a buffer to hold the data
+    std::vector<char> buffer(size);
+
+    // Read the file
+    inp.read(buffer.data(), size);
+
+    // print it for now
+    for (size_t i = 0; i < buffer.size(); i++) {
+            //std::cout << static_cast<double>(buffer[i]) << " ";
+            data.push_back(static_cast<double>(buffer[i]));
+        }
+        //std::cout << std::endl;
+
+    inp.close();
+
+    return 0;
+}
+
+
+
+// end main.cpp
